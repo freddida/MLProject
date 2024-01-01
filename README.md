69fd7831ea977f7bafb9dfb6c610543f85d1ac7b
freddida <frederik.dallomo@gmail.com>
2024-01-01 19:29:56 +0100

    added data loading script and requirements updated README

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-# MLProject
+# MLProject - v0.0.0 (unreleased)
 
-## Data
-The training and testing data can be found [here](https://kelvins.esa.int/collision-avoidance-challenge/data/).
+This repository contains a machine learning code to solve
+the [Collision Avoidance Challenge](https://kelvins.esa.int/collision-avoidance-challenge/home/) from ESA released in
+October 2019.
 
-Download and put it into the folder ```/data```
+## Clone
+
+* **HTTPS:** ```git clone https://github.com/freddida/MLProject.git```
+* **SSH:** ```git clonegit@github.com:freddida/MLProject.git```
+
+## Installation
+
+### Requirements
+* Run ```pip install -r requirements.txt```  to install all dependencies
+
+### Data
+* The training and testing data can be found [here](https://kelvins.esa.int/collision-avoidance-challenge/data/).
+* Download the training data and add it to the folder ```/data/training``` as ```train_data.csv```
+* Download the testing data and add it to the folder ```/data/testing``` as ```test_data.csv```
+
+## Usage
+
+## Before pushing to git
+
+* Run ```flake8 .``` for linting
+* Run ```black .``` for auto formatting
---
+++ src/data_loading.py
@@ -0,0 +1,75 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+
+class SpaceCollisionDataset(Dataset):
+    def __init__(self, csv_file, selected_columns=None, is_train=True):
+        if selected_columns is None:
+            selected_columns = []
+        selected_columns.extend(["risk"])
+
+        self.data = pd.read_csv(csv_file, usecols=selected_columns)
+        print("Columns in the dataset:", self.data.columns)
+        self.is_train = is_train
+
+        # Data Preprocessing
+        # All columns are numerical except for c_object_type: object type which is at collision risk with satellite
+        if "c_object_type" in self.data.columns:
+            self.label_encoder = LabelEncoder()
+            self.data["c_object_type"] = self.label_encoder.fit_transform(
+                self.data["c_object_type"]
+            )
+
+        # Handle Missing Values (replace with appropriate strategy if needed)
+        self.data = self.data.dropna()
+
+        # Split into features and targets for training
+        self.features = self.data.drop(["risk"], axis=1)
+        self.target = self.data["risk"]
+
+        if self.is_train:
+            # Split into train and validation sets
+            (
+                self.features_train,
+                self.features_val,
+                self.target_train,
+                self.target_val,
+            ) = train_test_split(
+                self.features, self.target, test_size=0.2, random_state=42
+            )
+
+            # Standardize numerical features
+            self.scaler = StandardScaler()
+            self.features_train_scaled = self.scaler.fit_transform(self.features_train)
+            self.features_val_scaled = self.scaler.transform(self.features_val)
+
+    def __len__(self):
+        if self.is_train:
+            return len(self.features_train)
+        else:
+            return len(self.data)
+
+    def __getitem__(self, idx):
+        features = torch.tensor(self.features.iloc[idx, :].values, dtype=torch.float32)
+        label = torch.tensor(self.target.iloc[idx], dtype=torch.float32)
+        return features, label
+
+
+if __name__ == "__main__":
+    columns_to_process = ["mission_id", "time_to_tca"]
+    train_dataset = SpaceCollisionDataset(
+        "../data/training/train_data.csv",
+        selected_columns=columns_to_process,
+        is_train=True,
+    )
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=16, shuffle=False, num_workers=2
+    )
+
+    # Print a few samples from the dataset
+    for i in range(5):
+        sample_features, sample_label = train_dataset[i]
+        print(f"Sample {i + 1} - Features: {sample_features}, Risk: {sample_label}")
