1b1fdb45e266cb824a09c3dcf0989d7fb193de18
freddida <frederik.dallomo@gmail.com>
2024-01-30 17:56:01 +0100

    model preperation

--- src/utils/calculate_statistics.py
+++ src/utils/calculate_statistics.py
@@ -2,7 +2,9 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, fbeta_score
 
 
-def calculate_f_beta_sklearn(y_true, y_pred, beta=2, threshold=-6):
+def calculate_statistics_for_evaluation(y_true, y_pred, beta=2, threshold=-6):
+    y_true = y_true.flatten()
+
     # Convert regression predictions to binary classification based on threshold
     y_pred_binary = (y_pred >= threshold).astype(int)
 
@@ -13,9 +15,10 @@ def calculate_f_beta_sklearn(y_true, y_pred, beta=2, threshold=-6):
     f_beta = fbeta_score(y_true_binary, y_pred_binary, beta=beta)
 
     # Calculate mean squared error for high-risk events
-    mse_hr = mean_squared_error(y_true[y_true >= threshold], y_pred[y_true >= threshold])
+    mse_hr = mean_squared_error(
+        y_true[y_true >= threshold], y_pred[y_true >= threshold]
+    )
 
-    # Calculate the final score
-    score = mse_hr / f_beta
+    l_score = mse_hr / f_beta
 
-    return score, f_beta, mse_hr
+    return f_beta, mse_hr, l_score
